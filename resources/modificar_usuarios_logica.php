78702c720f1833839244c6e0f7976e8ffd4f0054
David <pollo.david@outlook.com>
2018-10-30 18:26:57 -0600

    MiPrimerVercion

---
+++ resources/modificar_usuarios_logica.php
@@ -0,0 +1,40 @@
+<?php 
+include("../conexion.php");
+$idTrabajador_obtenido=$_POST["idTrabajador"];
+$contrasena_obtenido=$_POST["contrasena"];
+$tipo_obtenido=$_POST["tipo"];
+$estado_obtenido=$_POST["estado"];
+
+echo ("idTrabajador: ".$idTrabajador_obtenido."<br>");
+echo ("contrasena: ".$contrasena_obtenido."<br>");
+echo ("tipo: ".$tipo_obtenido."<br>");
+echo ("estado: ".$estado_obtenido."<br>");
+
+$server= servidor();
+
+$base = basedatos();
+
+$usuario=usuario();
+
+$contrasena=contrasena();
+
+$link = mysqli_connect($server,$usuario,$contrasena);
+mysqli_select_db($link,$base);
+
+//Revisando Consulta (query)
+$sql="UPDATE USUARIOS SET idTrabajador='".$idTrabajador_obtenido."', contrasena='".$contrasena_obtenido."',tipo='".$tipo_obtenido."', estado='".$estado_obtenido."' WHERE idTrabajador='".$idTrabajador_obtenido."'";
+
+if(mysqli_query($link,$sql)){
+  echo'<script type="text/javascript">
+    alert("Bien Modificado Con exito");
+    window.location.href="modificar_usuarios.php?idTrabajador=";
+    </script>';
+} else {
+
+  echo ("Modificacion Fallida  <br>");
+  echo "Error: " . $sql . "<br>" . mysqli_error($link);
+  
+}
+mysqli_close($link); //se cierra la conexion
+
+?>
---
+++ resources/registro_usuarios_funcion.php
@@ -0,0 +1,49 @@
+<?php 
+include("../conexion.php");
+//obteniendo valores de las cajas de texto
+$idTrabajador= htmlentities($_POST["idTrabajador"] );
+$contrasena= htmlentities($_POST["contrasena"] );
+$tipo= htmlentities($_POST["tipo"] );
+$estado= htmlentities($_POST["estado"] );
+
+
+
+
+//VISUALIZANDO VALORES
+echo("valores de las variables <br>");
+echo ("idTrabajador: ".$idTrabajador."<br>");
+echo ("contrasena: ".$contrasena."<br>");
+echo ("tipo: ".$tipo."<br>");
+echo ("estado: ".$estado."<br>");
+
+
+//Conectando con Base de Datos
+$server= servidor();
+
+$base = basedatos();
+
+$usuario=usuario();
+
+$contrasena=contrasena();
+
+$link = mysqli_connect($server,$usuario,$contrasena);
+mysqli_select_db($link,$base);
+
+//Revisando Consulta (query)
+$sql="INSERT INTO USUARIOS(idTrabajador, contrasena, tipo, estado) VALUES ('$idTrabajador', '$contrasena', '$tipo','$estado')";
+
+if(mysqli_query($link,$sql)){
+	echo'<script type="text/javascript">
+    alert("Registro Guardado");
+    window.location.href="registro_usuarios.php";
+    </script>';
+} else {
+
+	echo ("Resgistro Fallido <br>");
+	echo "Error: " . $sql . "<br>" . mysqli_error($link);
+	
+}
+mysqli_close($link); //se cierra la conexion
+
+
+?>
